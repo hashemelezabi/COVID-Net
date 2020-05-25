8b829fa3bd29fa035ead5c3b479483d35ce94bed
hashemelezabi <shoma98w@gmail.com>
2020-05-25 00:36:46 -0500

    evaluate model

--- jfh_transfer_v2.py
+++ jfh_transfer_v2.py
@@ -13,13 +13,26 @@ TOP_PERCENT = 0.08
 CHANNELS = 3
 
 def generate_dataset():
-	generator = BalanceCovidDataset(data_dir='../../COVID-NET/data',
+	generator = BalanceCovidDataset(data_dir='data',
 									csv_file='train_COVIDx2.txt',
 									batch_size=BATCH_SIZE,
 									input_shape=(IMG_SIZE, IMG_SIZE),
 									covid_percent=COVID_PERCENT,
 									class_weights=[1., 1., COVID_WEIGHT],
 									top_percent=TOP_PERCENT)
+
+	batch_x, batch_y, weights = next(generator)
+	return (batch_x, batch_y, weights)
+
+def generate_test_dataset():
+	generator = BalanceCovidDataset(data_dir='data',
+									csv_file='test_COVIDx2.txt',
+									batch_size=BATCH_SIZE,
+									input_shape=(IMG_SIZE, IMG_SIZE),
+									covid_percent=COVID_PERCENT,
+									class_weights=[1., 1., COVID_WEIGHT],
+									top_percent=TOP_PERCENT,
+									is_training=False)
 
 	batch_x, batch_y, weights = next(generator)
 	return (batch_x, batch_y, weights)
@@ -32,12 +45,14 @@ def load_base_model():
 
 def modify_base_model(base_model, learning_rate):
 	global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-	dense = tf.keras.layers.Dense(100)
+	dense1_layer = tf.keras.layers.Dense(100)
+	dense2_layer = tf.keras.layers.Dense(3)
 	prediction_layer = tf.keras.layers.Softmax()
 	model = tf.keras.Sequential([
 			  base_model,
 			  global_average_layer,
-			  dense,
+			  dense1_layer,
+			  dense2_layer,
 			  prediction_layer
 			])
 	model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=learning_rate),
@@ -45,11 +60,11 @@ def modify_base_model(base_model, learning_rate):
 		metrics=['accuracy'])
 	return model
 
-def evaluate_model(model):
+def evaluate_model(model, test_img_batch, test_label_batch):
 	initial_epochs = 10
 	validation_steps=20
-
-	loss0,accuracy0 = model.evaluate(validation_batches, steps = validation_steps)
+	print(test_label_batch.shape)
+	loss0,accuracy0 = model.evaluate(test_img_batch, test_label_batch, steps = validation_steps)
 	print("initial loss: {:.2f}".format(loss0))
 	print("initial accuracy: {:.2f}".format(accuracy0))
 
@@ -60,11 +75,18 @@ if __name__ == '__main__':
 
 	# global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 	# feature_batch_average = global_average_layer(batch_x)
+	# dense = tf.keras.layers.Dense(100)
+	# dense_batch = dense(feature_batch_average)
 	# prediction_layer = tf.keras.layers.Softmax()
-	# prediction_batch = prediction_layer(feature_batch_average)
+	# prediction_batch = prediction_layer(dense_batch)
 	# print(prediction_batch.shape)
+
 	model = modify_base_model(model, learning_rate=.0001)
-	print(model.summary())	
+	print(model.summary())
+
+	test_img_batch, test_label_batch, _ = generate_test_dataset()
+	evaluate_model(model, test_img_batch, test_label_batch)
+
 
 
 
